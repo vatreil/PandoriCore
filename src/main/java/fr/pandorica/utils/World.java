7a6fadccc0d56d511c20c459cc22b5a76cf39afd
waggo <valentin.treil@outlook.fr>
2023-02-24 20:28:15 +0100

    add pvp legacy

--- src/main/java/fr/pandorica/utils/World.java
+++ src/main/java/fr/pandorica/utils/World.java
@@ -1,17 +1,18 @@
 package fr.pandorica.utils;
 
 import net.minestom.server.MinecraftServer;
-import net.minestom.server.coordinate.Pos;
-import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.instance.AnvilLoader;
 import net.minestom.server.instance.DynamicChunk;
 import net.minestom.server.instance.IChunkLoader;
 import net.minestom.server.instance.InstanceContainer;
 
+
+
 public class World {
-    public static void load(GlobalEventHandler globalEventHandler, String path, Pos pos){
-            IChunkLoader loader = new AnvilLoader(path);
-            InstanceContainer instance = MinecraftServer.getInstanceManager().createInstanceContainer(loader);
-            instance.setChunkSupplier(DynamicChunk::new);
+    public static InstanceContainer instanceContainer;
+    public static void load(String path){
+        IChunkLoader loader = new AnvilLoader(path);
+        instanceContainer = MinecraftServer.getInstanceManager().createInstanceContainer(loader);
+        instanceContainer.setChunkSupplier(DynamicChunk::new);
     }
 }
