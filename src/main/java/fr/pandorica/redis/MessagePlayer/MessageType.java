84182965e0f155e3990e10d4290d8a1f495727ae
waggo <valentin.treil@outlook.fr>
2023-03-03 12:26:29 +0100

    add enum type message

---
+++ src/main/java/fr/pandorica/redis/MessagePlayer/MessageType.java
@@ -0,0 +1,39 @@
+package fr.pandorica.redis.MessagePlayer;
+
+import fr.pandorica.rank.RankManager;
+import fr.pandorica.redis.RedisInfoPlayer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum MessageType {
+
+    SEND_FRIEND(0, new RedisInfoPlayer()),
+    SEND_PARTY(2, new RedisInfoPlayer());
+
+    private int id;
+    private Object object;
+    public static Map<Integer, MessageType> ids = new HashMap<>();
+
+    private MessageType(int id, Object object) {
+        this.id = id;
+        this.object = object;
+    }
+
+    static {
+        for (MessageType type : MessageType.values()) {
+            ids.put(type.getId(), type);
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+
+
+}
