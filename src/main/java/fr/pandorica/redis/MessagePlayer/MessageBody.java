d81987ebf939c13ad80baab9aa7cb777f680d917
waggo <valentin.treil@outlook.fr>
2023-03-04 20:49:04 +0100

    add friend

--- src/main/java/fr/pandorica/redis/MessagePlayer/MessageBody.java
+++ src/main/java/fr/pandorica/redis/MessagePlayer/MessageBody.java
@@ -8,13 +8,14 @@ import java.util.UUID;
 
 public class MessageBody {
 
-    public static Map<String, String> getBody(MessageType messageType, UUID uuid_receiver,UUID uuid_sender, String msg){
+    public static Map<String, String> getBody(MessageType messageType, UUID uuid_receiver,UUID uuid_sender, String msg, String cmd){
         Map<String, String> messageBody = new HashMap<>();
         switch (messageType){
             case SEND_FRIEND:
                 messageBody.put("type", MessageType.SEND_PARTY.getId()+"");
                 messageBody.put("uuid", uuid_receiver.toString());
                 messageBody.put("sender_uuid", uuid_sender.toString());
+                messageBody.put("cmd", uuid_sender.toString());
                 messageBody.put("msg", msg);
         }
         return messageBody;
